1d31dd46b0981451c42b813a5a53a34b6211aaca
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2023-12-14 21:56:55 -0300

    Criando API para puxar dados no JS

--- controller/apiUsuario.php
+++ controller/apiUsuario.php
@@ -1,22 +1,29 @@
 <?php
 require_once("bancoConexao.php");
 header("Content-Type: application/json");
+session_start();
+    if(isset($_SESSION["email"])){
+        $email = $_SESSION['email'];
+        $sql = "SELECT codUsuario, nome, email FROM usuario where email = '$email'";
+        $valores = $conexao->consultaBanco($sql);
 
-$sql = "SELECT codUsuario, nome, email FROM usuario where codUsuario = 13";
-$valores = $conexao->consultaBanco($sql);
+        $jsonArray = array();
 
-$jsonArray = array();
+        foreach ($valores as $dados){
+            $jsonArray = [
+                "nome"=> $dados["nome"], 
+                "id"=> $dados["codUsuario"],
+                "email"=> $dados["email"]
+            ];
+            echo json_encode($jsonArray, JSON_PRETTY_PRINT) . "\n";
+        };
 
-foreach ($valores as $dados){
+    }else{
+        echo json_encode("voce so tera acesso se estiver logado");
+        exit();
+    }
 
-    $jsonArray = [
-        "nome"=> $dados["nome"], 
-        "id"=> $dados["codUsuario"],
-        "email"=> $dados["email"]
-    ];
 
-    echo  json_encode($jsonArray, JSON_PRETTY_PRINT) . "\n";
-};
 
 
 ?>
--- view/js/home.js
+++ view/js/home.js
@@ -20,6 +20,13 @@ editarEmail.addEventListener('click',()=>{
     label.htmlFor='editarEmail'
     label.textContent='Digite o seu novo email:'
     form.appendChild(label)
+    fetch('../../controller/apiUsuario.php', {
+        method: 'GET',
+    })
+    .then(response => response.json())
+    .then(data => {
+        input.value= data['email']
+    })
     input.type='text'
     input.name='editarEmail'
     form.appendChild(input)
@@ -54,12 +61,21 @@ editarNome.addEventListener('click',()=>{
     section.appendChild(form)
     section.classList.add('editar-dados')
 
-    label.htmlFor='editarEmail'
+    label.htmlFor='editarNome'
     label.textContent='Digite o seu nome:'
     form.appendChild(label)
     input.type='text'
+
     input.name='editarNome'
     form.appendChild(input)
+
+    fetch('../../controller/apiUsuario.php', {
+        method: 'GET',
+    })
+    .then(response => response.json())
+    .then(data => {
+        input.value= data['nome']
+    })
 
     button.textContent='Alterar'
     button.type='submit'
