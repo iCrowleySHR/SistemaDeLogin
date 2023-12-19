52ca801f2b4ebb142dc61698d0859b7b46e4fc9b
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2023-12-19 16:05:27 -0300

    Estilizando a página de cadastro

--- view/components/formulario.php
+++ view/components/formulario.php
@@ -1,16 +1,17 @@
 <?php include '../../controller/cadUsuario.php'; ?>
-<form action="" method='post'>
+<form method='post'>
     <section>
         <h2>Cadastre-se no nosso site!</h2>
-            <label for="name">Nome</label>
+            <label for="name">Nome:</label>
             <input type="text" name="name" required maxlength="100"> 
 
-            <label for="email">E-mail</label>
+            <label for="email">E-mail:</label>
             <input type="text" name="email" required maxlength="100" id="email">
             <span id="error-email"></span>
 
-            <label for="senha">Senha</label>
-            <input type="password" name="password" required maxlength="100">
+            <label for="senha">Senha:</label>
+            <input type="password" name="password" id='senha' required maxlength="100">
+            <img src="../img/olhoAberto.png" alt="" class="olho">
             
         <button type="submit" id="btnEnviar">Enviar</button>
     </section>
--- view/js/script.js
+++ view/js/script.js
@@ -2,16 +2,36 @@ let inputEmail = document.querySelector('#email');
 let spanError = document.querySelector('#error-email')
 let btnEnviar = document.querySelector('#btnEnviar')
 
+
 inputEmail.addEventListener('input', ()=>{
     let emailValue = inputEmail.value
     let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if(!emailRegex.test(emailValue)){
         spanError.style.color='red'
-        spanError.style.marginLeft='-76px'
         spanError.innerHTML=`Email invalido!`
         btnEnviar.disabled = true
+        btnEnviar.style.backgroundColor='gray'
     }else{
         spanError.innerHTML=``
         btnEnviar.disabled = false
+        btnEnviar.style.backgroundColor='rgb(1, 105, 190)'
     }
 })
+
+const imgOlho = document.querySelector('.olho')
+const inputSenha = document.querySelector('#senha')
+olho = true;
+
+function sistemaOlho(){
+    if(olho == true){
+        imgOlho.src='../img/olhoFechado.png'
+        inputSenha.type='text'
+        olho = false
+    }else{
+        imgOlho.src='../img/olhoAberto.png'
+        inputSenha.type='password'
+        olho = true
+    }
+}
+
+imgOlho.addEventListener('click',sistemaOlho)
