4e568056786470a3835570fc90b87bf62224aeb2
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2023-12-19 22:19:47 -0300

    update

--- view/components/inputLogin.php
+++ view/components/inputLogin.php
@@ -2,11 +2,11 @@
 <form action="" method='post'>
     <section>
         <h2>Acesse a sua conta!</h2>
-            <label for="email">E-mail</label>
+            <label for="email">E-mail:</label>
             <input type="text" name="email" id="email" required maxlength="100">
             <span id="error-email"></span>
 
-            <label for="senha">Senha</label>
+            <label for="senha">Senha:</label>
             <input type="password" name="password" id="senha" required maxlength="100" autocomplete="current-password" >
             <img src="../img/olhoAberto.png" alt="" class="olho">
         <button type="submit" id="btnEnviar">Enviar</button>
